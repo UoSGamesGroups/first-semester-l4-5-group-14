7b80875ee26acb6e955f2d290b52007b2dbf8db0
Ryan Jake Manthorp <ryanmanthorp@hotmail.co.uk>
2016-11-07 23:20:26 +0000

    Added start screen

---
+++ Unity Folder/Group 14/Assets/Scripts (scr)/menue_script.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class menue_script : MonoBehaviour
+{
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+
+            print("pressed");
+            SceneManager.LoadScene("scn_mainNew");
+
+        }
+    }
+
+}
