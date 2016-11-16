e0508d5b95a509d96a81e375048c2cb1606c2fbc
Daniel Pokladek <danielpokladek@gmail.com>
2016-11-16 00:43:52 +0000

    Added my slide for presentation.

---
+++ Unity Folder/Group 14/Assets/Scripts (scr)/scr_DrawOutline.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_DrawOutline : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGizmosDraw () {
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube(transform.position, new Vector3(0.5f,2f,1f));
+	}
+}
