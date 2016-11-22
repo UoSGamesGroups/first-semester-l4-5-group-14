b00b32624d9ec5dc5424ca132d13a37425758a75
Ryan Jake Manthorp <ryanmanthorp@hotmail.co.uk>
2016-11-22 17:34:19 +0000

    Fixed some bugs

--- Unity Folder/Group 14/Assets/Scripts (scr)/mouseClick.cs
+++ Unity Folder/Group 14/Assets/Scripts (scr)/mouseClick.cs
@@ -4,18 +4,30 @@ using System.Collections;
 public class mouseClick : MonoBehaviour {
 
     	float distance = 11f;
+        float speed = 1f;
 
-	void OnMouseDrag() {
+        float rotSpeed = 10f;
+
+
+    void OnMouseDrag() {
      		Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
         	Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-        	transform.position = objPosition;
+             float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+             float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+
+             transform.Rotate(Vector3.up, -rotX);
+             transform.Rotate(Vector3.right, rotY);
+
+                transform.position = objPosition;
     	}
 
 	void Update () {
 		if (Input.GetMouseButton(0)) {
 			scr_gameManager.GameManager.isDragging = true;
-		} else {
+      
+
+        } else {
 			scr_gameManager.GameManager.isDragging = false;
 		}
 	}
